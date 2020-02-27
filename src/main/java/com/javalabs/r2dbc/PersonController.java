00681d5d417e31fdd9d23ba7041cbe91d1c7ee58
Pedro Alexandre Tavares <patavares@outlook.com>
2020-02-27 04:02:56 +0000

    Remove api from resource

--- src/main/java/com/javalabs/r2dbc/PersonController.java
+++ src/main/java/com/javalabs/r2dbc/PersonController.java
@@ -6,7 +6,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/api/person")
+@RequestMapping("/person")
 @RequiredArgsConstructor
 public class PersonController {
     private final PersonRepository repository;
